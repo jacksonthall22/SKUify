0ba294815a13e30e795470e3d7688887c3567191
Jackson Hall <jacksonthall22@gmail.com>
2019-09-15 02:08:27 -0400

    Added welcome()

---
+++ skuify.py
@@ -0,0 +1,12 @@
+def main():
+    welcome()
+
+def welcome():
+        print('Welcome to Amazon FBA Seller SKU builder!')
+        print('v1.0')
+        print('Created by Jackson Hall')
+        print()
+        print()
+
+if __name__ == '__main__':
+    main()
